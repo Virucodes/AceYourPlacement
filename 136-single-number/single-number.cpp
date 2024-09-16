0d444b083d9fc24c90badbad01b79922c9ba2279
Viraj <162709833+Virucodes@users.noreply.github.com>
2024-09-17 00:58:20 +0530

    Time: 16 ms (41.22%) | Memory: 20.8 MB (18.57%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,16 +3,27 @@ public:
     int singleNumber(vector<int>& nums) {
         unordered_map<int,int> map(nums.size());
 
-        for(int i=0;i<nums.size();i++)
-        {
-            map[nums[i]]++;
-        }
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     map[nums[i]]++;
+        // }
 
-        for(auto &pair:map)
-        {
-            if(pair.second==1) return pair.first;
-        }
+        // for(auto &pair:map)
+        // {
+        //     if(pair.second==1) return pair.first;
+        // }
 
-        return -1;
+        // return -1;
+
+        
+         int Xor = 0;
+
+         for(int i=0;i<nums.size();i++)
+         {
+            Xor ^= nums[i];
+         }
+
+       
+        return Xor;
     }
 };
