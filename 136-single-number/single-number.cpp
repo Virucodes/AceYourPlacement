e687f5cff3a5853491b00f73b0d9020c03a083a8
Viraj <162709833+Virucodes@users.noreply.github.com>
2024-09-17 00:52:16 +0530

    Time: 18 ms (36.84%) | Memory: 23.8 MB (15.52%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> map(nums.size());
+
+        for(int i=0;i<nums.size();i++)
+        {
+            map[nums[i]]++;
+        }
+
+        for(auto &pair:map)
+        {
+            if(pair.second==1) return pair.first;
+        }
+
+        return -1;
+    }
+};
