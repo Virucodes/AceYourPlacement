14d3f5a4c6b4a4e8ecc58e890bd2a2455bcf25aa
Viraj <162709833+Virucodes@users.noreply.github.com>
2024-11-12 00:12:04 +0530

    Time: 3 ms (67.88%) | Memory: 26.7 MB (87.22%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -1,33 +1,43 @@
-
 class Solution {
 public:
-    bool checkPrime(int x) {
-        if (x <= 1) return false;
-        for (int i = 2; i <= sqrt(x); i++) {
-            if (x % i == 0) {
-                return false;
-            }
+    bool isPrime(int num)
+    {
+        if(num<=1) return false;
+        for(int i=2;i<=sqrt(num);i++)
+        {
+            if(num%i==0)return false;
         }
         return true;
     }
 
+    int largestPrime(int n)
+    {
+        for(int i=n-1;i>=2;i--)
+        {
+            if(isPrime(i)) return i;
+        }
+        return -1;
+    }
     bool primeSubOperation(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            int bound = (i == 0) ? nums[i] : nums[i] - nums[i - 1];
-            
-            if (bound <= 0) return false;
-            
-            int largestPrime = 0;
+        
+        
+        for(int i=0;i<nums.size();i++)
+        {
+             int bound = (i==0)?nums[i]:nums[i]-nums[i-1];
+
+             if(bound<=0)return false;
+
+              int largestPrime = 0;
             for (int j = bound - 1; j >= 2; j--) {
-                if (checkPrime(j)) {
+                if (isPrime(j)) {
                     largestPrime = j;
                     break;
                 }
             }
-            
-            nums[i] -= largestPrime;
+             nums[i]-=largestPrime;
         }
-        
         return true;
     }
+
+
 };
