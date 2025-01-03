62c86b416df7b19f32d5905d83c997b58dabbf9b
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-01-03 17:13:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (33.17%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     string interpret(string command) {
-        
         string result="";
-
         for(int i=0;i<command.size();i++){
-
-        if(command[i]=='G') result.push_back('G');
-        if(i<command.size() && command[i]=='(' && command[i+1]==')') result.push_back('o');
-        if(i< command.size()&& command[i]=='(' && command[i+1]!=')') result.append("al");
-
+            if(command[i]=='G') result.push_back('G');
+            if(command[i]=='(')
+            {
+                if(i<command.size() && command[i+1]==')')
+                {
+                    result.push_back('o');
+                }
+                else 
+                result.append("al");
+            }
         }
+             
+        
         return result;
     }
 };
