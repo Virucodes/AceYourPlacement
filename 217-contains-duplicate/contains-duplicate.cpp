4c1b269f6254c6abbbd4018433c4efd9b247ba46
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-01-10 17:24:59 +0530

    Time: 29 ms (68.83%) | Memory: 73.6 MB (43.73%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,10 +3,18 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         
        
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            if(nums[i]==nums[i+1]) return true;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size()-1;i++)
+        // {
+        //     if(nums[i]==nums[i+1]) return true;
+        // }
+        // return false;
+
+        unordered_map<int,int> mp;
+
+        for(int num:nums){
+            mp[num]++;
+            if(mp[num]>1) return true;
         }
         return false;
     }
