d6059b6d82cd2166e8d4b32e0b00498808d2706d
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-01-14 18:21:30 +0530

    Time: 14 ms (79.47%) | Memory: 24.9 MB (73.69%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector<vector<string>> ans;
-
         unordered_map<string,vector<string>> mp;
         for(auto &str: strs)
         {
@@ -11,12 +11,10 @@ public:
             sort(str.begin(),str.end());
             mp[str].push_back(original);
         }
-
         for(auto &pair: mp)
         {
             ans.push_back(pair.second);
         }
-
         return ans;
     }
 };
