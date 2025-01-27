e308711d17e00ae63b93502e95ff65f2ffb7c329
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-01-27 12:06:24 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (94.96%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -2,49 +2,49 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         
-        // int totalSum = 0;
+        int totalSum = 0;
 
-        // for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
 
-        //     totalSum += nums[i];
-        // }
-
-        // int leftSum = 0;
-        // int rightSum =0;
-
-        // for(int i=0;i<nums.size();i++){
-
-            
-        //     rightSum = totalSum - leftSum;
-
-        //     if(leftSum == rightSum){
-        //         return i;
-        //     }
-        //     else{
-        //         leftSum += nums[i];
-        //     }
-            
-
-        // }
-
-        // return -1;
-
-        int n = nums.size();
-        vector<int> ans(n,0);
-        int leftSum = 0;
-        int rightSum = 0;
-
-        for(int i=n-1;i>=0;i--){
-            ans[i] += rightSum;
-            rightSum += nums[i];
+            totalSum += nums[i];
         }
-        for(int i=0;i<n;i++){
 
-            if(leftSum == ans[i]){
+        int leftSum = 0;
+        int rightSum =0;
+
+        for(int i=0;i<nums.size();i++){
+
+            
+            rightSum = totalSum - leftSum - nums[i];
+
+            if(leftSum == rightSum){
                 return i;
             }
-            leftSum +=nums[i];
+            else{
+                leftSum += nums[i];
+            }
+            
+
         }
+
         return -1;
+
+        // int n = nums.size();
+        // vector<int> ans(n,0);
+        // int leftSum = 0;
+        // int rightSum = 0;
+
+        // for(int i=n-1;i>=0;i--){
+        //     ans[i] += rightSum;
+        //     rightSum += nums[i];
+        // }
+        // for(int i=0;i<n;i++){
+
+        //     if(leftSum == ans[i]){
+        //         return i;
+        //     }
+        //     leftSum +=nums[i];
+        // }
+        // return -1;
     }
 };
