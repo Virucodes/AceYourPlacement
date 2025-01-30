eeea471c81a2d2e81320789370b11436d47eea96
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-01-30 13:11:28 +0530

    Time: 3 ms (98.21%) | Memory: 11.3 MB (83.54%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -10,17 +10,48 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         
-        unordered_map<ListNode*,int> mp;
-        int i = 0;
-        while(head != nullptr){
-            if(mp.find(head) != mp.end()){
-                return head;
-            }
-            mp[head] = 0;
-            head = head->next;
+        // unordered_map<ListNode*,int> mp;
+        // int i = 0;
+        // while(head != nullptr){
+        //     if(mp.find(head) != mp.end()){
+        //         return head;
+        //     }
+        //     mp[head] = 0;
+        //     head = head->next;
+        // }
+
+        // return nullptr;
+
+
+        //Another approach to solve using the tortoise and hare
+
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        bool flag = false;
+
+        while(fast && fast->next){
+             
+             slow = slow->next;
+             fast = fast->next->next;
+             if(fast == slow){
+                 flag = true;
+                 break;
+             }
         }
 
-        return nullptr;
+        if(!flag){ return nullptr;}
+
+
+        slow = head;
+
+
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
+
+        }
+        return slow;
 
     }
 };
