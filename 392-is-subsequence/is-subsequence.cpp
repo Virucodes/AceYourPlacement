2d57fa4293b06a0ab4c24489b7bca8b5e2ff929b
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-01-31 21:04:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.96%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -2,40 +2,33 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         
-        // int i = 0;
-        // int j = 0;
+        int i = 0;
+        int j = 0;
 
-        // if(s.length() < 1 && t.length() < 1){
-        //     return true;
-        // }
-
-        // if(s.length() > t.length()){
-        //     return false;
-        // }
+        int m = t.length();
+        int n = s.length();
 
        
-        // while(i < t.length()){
-        //     if( j< s.length() && s[j] == t[i]){
-                
-        //         i++;
-        //         j++;
-        //     }
+        while(i < m){
+            if( j < n && s[j] == t[i]){
+                j++;
+            }
             
-        //     i++;
-        // }
-        // return j == s.length();
-
-         int i = 0, j = 0;
-    int n = s.length(), m = t.length();
-    
-    while (j < m) {
-        if (i < n && s[i] == t[j]) {
-            i++;  // Move to the next character in s1
+            i++;
         }
-        j++;  // Always move in s2
-    }
+        return j == n;
+
+    //      int i = 0, j = 0;
+    // int n = s.length(), m = t.length();
     
-    return i == n;
+    // while (j < m) {
+    //     if (i < n && s[i] == t[j]) {
+    //         i++;  // Move to the next character in s1
+    //     }
+    //     j++;  // Always move in s2
+    // }
+    
+    // return i == n;
         
     }
 };
