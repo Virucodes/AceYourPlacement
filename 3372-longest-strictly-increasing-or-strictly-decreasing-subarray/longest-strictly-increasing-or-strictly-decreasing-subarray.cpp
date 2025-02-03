d170ea6ea3fbbdcdd4912baf34baab05de8abb8e
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-02-03 11:38:31 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (27.06%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -2,41 +2,77 @@ class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
         
-        int i=0;
-        int maxele = 1;
+    //     int i=0;
+    //     int maxele = 1;
 
-        if(nums.size() == 1){
+    //     if(nums.size() == 1){
+    //         return 1;
+    //     }
+    //     int count = 1;
+    //     for(int i=1;i<nums.size();i++){
+
+    //         if(nums[i] > nums[i-1])
+    //         {
+    //             count++;
+    //         }
+    //         else{
+    //             count = 1;
+    //         }
+    //         maxele = max(count,maxele);
+
+    //     }
+    //    count = 1;
+    //     for(int i=nums.size()-2;i>=0;i--){
+
+    //         if(nums[i] > nums[i+1])
+    //         {
+    //             count++;
+    //         }
+    //         else{
+    //             count = 1;
+    //         }
+    //         maxele = max(count,maxele);
+
+    //     }
+
+
+    //     return maxele;
+
+        if(nums.size() ==1){
             return 1;
         }
-        int count = 1;
-        for(int i=1;i<nums.size();i++){
+        int INC_MAX  = 1;
+        int DEC_MAX = 1;
 
-            if(nums[i] > nums[i-1])
-            {
-                count++;
-            }
-            else{
-                count = 1;
-            }
-            maxele = max(count,maxele);
+        int inc = 1;
+        int dec = 1;
+
+        for(int i = 0;i<nums.size()-1;i++){
+             
+             if(nums[i] < nums[i+1])
+             {
+                inc++;
+                INC_MAX = max(INC_MAX, inc);
+             }
+             else{
+                
+                inc = 1;
+             }
+             if(nums[i] > nums[i+1])
+             {
+                dec++;
+                DEC_MAX = max(DEC_MAX, dec);
+             }
+             else{
+                
+                dec = 1;
+             }
 
         }
-       count = 1;
-        for(int i=nums.size()-2;i>=0;i--){
 
-            if(nums[i] > nums[i+1])
-            {
-                count++;
-            }
-            else{
-                count = 1;
-            }
-            maxele = max(count,maxele);
-
-        }
+        return max(INC_MAX, DEC_MAX);
 
 
-        return maxele;
 
        
     }
