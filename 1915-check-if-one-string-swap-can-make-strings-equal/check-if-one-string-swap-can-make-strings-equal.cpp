230b6f226724b7aa492bb8ff36dde2dead2f386e
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-02-05 12:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (36.33%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
+        // if (s1 == s2) {
+        //     return true;
+        // }
+        // int mismatch = 0;
+        // map<char, int> mp1;
+        // map<char, int> mp2;
+        // bool done = false;
+
+        // for (int i = 0; i < s1.length(); i++) {
+
+        //     mp1[s1[i]]++;
+        //     mp2[s2[i]]++;
+        //     if (s1[i] != s2[i]) {
+        //         mismatch++;
+        //         char a, b;
+        //         if (mismatch == 1) {
+        //             a = s1[i];
+        //             b = s2[i];
+        //         }
+        //         if (mismatch == 2) {
+
+        //             if (s1[i] == b && s2[i] == a) {
+        //                 done = true;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // if (mismatch == 0 || (mismatch == 2 && done)) {
+        //     return true;
+        // }
+
+        // return false;
+
+
+        if (s1.size() != s2.size()) return false;
         
-            
-            if(s1 == s2){
-                return true;
+        vector<int> cnt1(26, 0), cnt2(26, 0);
+        for (char c : s1) cnt1[c - 'a']++;
+        for (char c : s2) cnt2[c - 'a']++;
+        if (cnt1 != cnt2) return false;
+        
+        vector<int> diff;
+        for (int i = 0; i < s1.size(); ++i) {
+            if (s1[i] != s2[i]) {
+                diff.push_back(i);
+                if (diff.size() > 2) return false;
             }
-
-
-            int mismatch = 0;
-
-            map<char, int> mp1;
-            map<char, int> mp2;
-
-            
-            bool done = false;
-
-            for(int i =0;i<s1.length();i++){
-
-                mp1[s1[i]]++;
-                mp2[s2[i]]++;
-                if(s1[i] != s2[i])
-                {
-                    mismatch++;
-                    char a,b;
-                    if(mismatch==1){
-                        a = s1[i];
-                        b = s2[i];
-                    }
-                    if(mismatch==2){
-                        
-                        if(s1[i] == b && s2[i] == a){
-                            done = true;
-                        }
-                    }
-                }
-            }
-
-            if(mismatch == 0 || (mismatch==2 && done)){
-                return true;
-            }
-
-            return false;
-
-           
-
-
-
-          
-
+        }
+        
+        return diff.empty() || (diff.size() == 2 && s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]]);
     }
 };
