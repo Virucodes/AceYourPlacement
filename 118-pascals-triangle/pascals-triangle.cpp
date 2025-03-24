99f612c6071cc56961e7e8f8723970c1d115d23f
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-03-24 15:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (65.10%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -2,23 +2,47 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         
-        if(numRows == 0) return {};
 
-        if(numRows == 1) return {{1}};
+        //using recursion
+        // if(numRows == 0) return {};
+
+        // if(numRows == 1) return {{1}};
 
 
-        vector<vector<int>> prevRows = generate(numRows - 1);
+        // vector<vector<int>> prevRows = generate(numRows - 1);
 
-        vector<int> newRow(numRows,1);
+        // vector<int> newRow(numRows,1);
 
-        for(int i=1;i < numRows -1; i++){
+        // for(int i=1;i < numRows -1; i++){
 
-            newRow[i] = prevRows.back()[i-1] + prevRows.back()[i];
+        //     newRow[i] = prevRows.back()[i-1] + prevRows.back()[i];
 
+        // }
+
+        // prevRows.push_back(newRow);
+        // return prevRows;
+        
+
+
+        //using combinatinal formula
+
+
+        vector<vector<int>> result;
+
+
+        for(int i=0;i < numRows;i++)
+        {
+            vector<int> row(i+1,1);
+
+            for(int j=1;j<i;j++)
+            {
+                row[j] = result[i-1][j-1] + result[i-1][j];
+            }
+
+            result.push_back(row);
         }
 
-        prevRows.push_back(newRow);
-        return prevRows;
+        return result;
 
     }
 };
