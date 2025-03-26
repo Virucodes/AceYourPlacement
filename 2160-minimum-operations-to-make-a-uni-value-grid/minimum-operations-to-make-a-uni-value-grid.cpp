549f14fa844d3399af8fc9fdca810ad6700cbd15
Viraj <162709833+Virucodes@users.noreply.github.com>
2025-03-26 08:19:29 +0530

    Time: 52 ms (27.96%) | Memory: 90.3 MB (20.97%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<int> total;
-
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                total.push_back(grid[i][j]);
+        vector<int> arr;
+        for (auto& row : grid) {
+            for (int num : row) {
+                arr.push_back(num);
             }
         }
-
         
+        sort(arr.begin(), arr.end());
+        int median = arr[arr.size() / 2];
+        int operations = 0;
 
-        for(int num: total)
-        {
-            if((num-total[0]) % x !=0) return -1;
+        for (int num : arr) {
+            int diff = abs(num - median);
+            if (diff % x != 0) return -1;
+            operations += diff / x;
         }
-
-        sort(total.begin(),total.end());
-        
-
-        int median = total[total.size()/2];
-        
-
-        int count = 0;
-        for(int i=0;i<total.size();i++)
-        {
-
-            count += abs(median-total[i])/x;
-
-            cout<<total[i] << " " <<count<<endl;
-        }
-        return count;
+        return operations;
     }
 };
